d1c9a1d34d9525a50e0e3c11b275ef4653e21dcb
Heloriel <andradematheus.dev@gmail.com>
2023-03-22 20:43:16 -0300

    Navigation anchors

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,11 +1,28 @@
-import { BsClipboard, BsFileEarmarkText, BsGear, BsPeople } from 'react-icons/bs';
+import { BsClipboard, BsFileEarmarkText, BsGear, BsPeople } from "react-icons/bs";
 export const Navigation = () => {
   return (
     <ul className="nav-links text-white">
-      <li><BsClipboard /> <span>Boards</span></li>
-      <li><BsPeople /> <span>Equipes</span></li>
-      <li><BsFileEarmarkText /> <span>Relatórios</span></li>
-      <li><BsGear /><span>Ajustes</span></li>
+      <li>
+        <a href="#">
+          <BsClipboard /> <span>Boards</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <BsPeople /> <span>Equipes</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <BsFileEarmarkText /> <span>Relatórios</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <BsGear />
+          <span>Ajustes</span>
+        </a>
+      </li>
     </ul>
-  )
-}
+  );
+};
