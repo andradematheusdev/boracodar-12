3f617a7c17f703d860596f610877d606fd313176
Heloriel <andradematheus.dev@gmail.com>
2023-03-22 16:13:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="wrapper">
+      <nav className="flex w-2/12 items-center justify-center bg-purple-600">
+        aaa
+      </nav>
+      <div className="flex w-10/12 bg-purple-600">
+        <div className="flex w-full items-center justify-center bg-white rounded-tl-3xl">
+          aaaa
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
