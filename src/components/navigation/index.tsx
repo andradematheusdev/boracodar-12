76ae15a513a38bad5ee97357e81276c79688f3a6
Heloriel <andradematheus.dev@gmail.com>
2023-03-22 17:02:36 -0300

    Navigation icons

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,10 +1,11 @@
+import { BsClipboard, BsFileEarmarkText, BsGear, BsPeople } from 'react-icons/bs';
 export const Navigation = () => {
   return (
-    <ul className="text-white">
-      <li>Boards</li>
-      <li>Equipes</li>
-      <li>Relatórios</li>
-      <li>Ajustes</li>
+    <ul className="nav-links text-white">
+      <li><BsClipboard /> <span>Boards</span></li>
+      <li><BsPeople /> <span>Equipes</span></li>
+      <li><BsFileEarmarkText /> <span>Relatórios</span></li>
+      <li><BsGear /><span>Ajustes</span></li>
     </ul>
   )
 }
