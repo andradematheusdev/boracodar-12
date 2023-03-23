07f5b74c8adf98b894a769c45118c93c974f779c
Heloriel <andradematheus.dev@gmail.com>
2023-03-23 10:27:56 -0300

    Fix: grid gap

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -2,16 +2,18 @@ import { Card } from "../card"
 
 export const Grid = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-1 text-neutral-700 py-8">
-      <div>
+    <div className="grid grid-cols-3 grid-rows-1 text-neutral-700 py-8 gap-x-12">
+      <div className="">
         <span className="block text-xl font-bold mb-6">A Fazer</span>
         <Card />
       </div>
-      <div>
+      <div className="">
         <span className="block text-xl font-bold mb-6">Fazendo</span>
+        <Card />
       </div>
-      <div>
+      <div className="">
         <span className="block text-xl font-bold mb-6">Feito</span>
+        <Card />
       </div>
     </div>
   )
