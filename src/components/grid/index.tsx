37e8be89c4225dc75e78111cf38408a3e68d4aef
Heloriel <andradematheus.dev@gmail.com>
2023-03-23 10:15:08 -0300

    Grid padding y

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,6 +1,6 @@
 export const Grid = () => {
   return (
-    <div className="grid grid-cols-3 grid-rows-1 text-neutral-700">
+    <div className="grid grid-cols-3 grid-rows-1 text-neutral-700 py-8">
       <div>
         <span className="text-xl font-bold">A Fazer</span>
       </div>
