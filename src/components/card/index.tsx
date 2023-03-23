40720c28c3e90d63e914edca3e4012908a2199f6
Heloriel <andradematheus.dev@gmail.com>
2023-03-23 12:16:19 -0300

    Style: card shadow on hover

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,7 +2,7 @@ import { Tag } from "../tag"
 
 export const Card = () => {
   return (
-    <div className="bg-white shadow-xl shadow-violet-50 p-6 rounded-lg">
+    <div className="bg-white hover:shadow-md hover:shadow-violet-100 p-6 rounded-xl transition-all">
       <h3 className="font-bold mb-3">#boraCodar um Kanban 🧑‍💻</h3>
       <p className="mb-3">Novo desafio do #boraCodar da Rocketseat, onde é proposto construir um quadro de Kanban.</p>
       <div>
