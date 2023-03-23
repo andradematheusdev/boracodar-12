8a2fd7686bdd4e770c14ffb19f79ecfa44110e7b
Heloriel <andradematheus.dev@gmail.com>
2023-03-23 12:14:19 -0300

    Tag component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,10 +1,12 @@
+import { Tag } from "../tag"
+
 export const Card = () => {
   return (
     <div className="bg-white shadow-xl shadow-violet-50 p-6 rounded-lg">
       <h3 className="font-bold mb-3">#boraCodar um Kanban 🧑‍💻</h3>
       <p className="mb-3">Novo desafio do #boraCodar da Rocketseat, onde é proposto construir um quadro de Kanban.</p>
       <div>
-        aaa
+        <Tag />
       </div>
     </div>
   )
---
+++ src/components/tag/index.tsx
@@ -0,0 +1,7 @@
+export const Tag = () => {
+  return (
+    <span className="bg-violet-200 rounded-lg py-1 px-2 text-sm text-violet-600">
+      index
+    </span>
+  )
+}
