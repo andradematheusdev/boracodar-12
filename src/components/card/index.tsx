16fc56fe78df18c6e974ec8b51cc0ba3a06ba2c3
Heloriel <andradematheus.dev@gmail.com>
2023-03-23 10:25:59 -0300

    Card component base

---
+++ src/components/card/index.tsx
@@ -0,0 +1,11 @@
+export const Card = () => {
+  return (
+    <div className="bg-white shadow-xl shadow-violet-100 p-6 rounded-lg">
+      <h3 className="font-bold mb-3">#boraCodar um Kanban 🧑‍💻</h3>
+      <p className="mb-3">Novo desafio do #boraCodar da Rocketseat, onde é proposto construir um quadro de Kanban.</p>
+      <div>
+        aaa
+      </div>
+    </div>
+  )
+}
--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,14 +1,17 @@
+import { Card } from "../card"
+
 export const Grid = () => {
   return (
     <div className="grid grid-cols-3 grid-rows-1 text-neutral-700 py-8">
       <div>
-        <span className="text-xl font-bold">A Fazer</span>
+        <span className="block text-xl font-bold mb-6">A Fazer</span>
+        <Card />
       </div>
       <div>
-        <span className="text-xl font-bold">Fazendo</span>
+        <span className="block text-xl font-bold mb-6">Fazendo</span>
       </div>
       <div>
-        <span className="text-xl font-bold">Feito</span>
+        <span className="block text-xl font-bold mb-6">Feito</span>
       </div>
     </div>
   )
