6e7e54e1c95504a57844fb4a74bec7ac2267d49e
Heloriel <andradematheus.dev@gmail.com>
2023-03-24 15:26:49 -0300

    Style: search bar white background

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -7,13 +7,13 @@ export const Search = () => {
         <BsFilter size={22} />
         <span>Filtrar</span>
       </button>
-      <div className="flex flex-1 items-center border border-neutral-200 rounded-lg overflow-hidden px-7">
+      <div className="flex flex-1 items-center border border-neutral-200 rounded-lg overflow-hidden px-7 bg-white">
         <BsSearch size={18} />
         <input
           type="text"
           name="search"
           id="search-input"
-          className="flex-1 focus:outline-none px-3 bg-transparent"
+          className="flex-1 focus:outline-none px-3 bg-transparent bg-white"
           placeholder="Busque por cards, assuntos ou responsáveis..."
         />
       </div>
