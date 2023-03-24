9ee90af6ceb767d471799c35f49dbd6c3dbcfe68
Heloriel <andradematheus.dev@gmail.com>
2023-03-24 16:39:03 -0300

    Edit board name

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,31 @@
+import clsx from "clsx";
+import { useState } from "react";
 import { BsPencilFill } from "react-icons/bs"
 
 export const Header = () => {
+  const [title, setTitle] = useState("Meu Kanban");
+  const [readOnly, setReadOnly] = useState(true);
+
+  function handleEdit(){
+    setReadOnly(!readOnly);
+  }
+
+  function handleTitleChange(text: string){
+    setTitle(text)
+  }
+
   return (
     <header className="flex w-full justify-between items-center px-6 py-3">
       <div className="flex flex-1 gap-x-2 items-center">
-        <h1 className="text-4xl font-bold text-neutral-700">Meu Kanban</h1>
+        <input
+          type={"text"}
+          className={clsx({"underline": !readOnly},"flex w-auto text-4xl font-bold text-neutral-700 bg-transparent focus:outline-none")}
+          value={title}
+          onChange={(e)=>handleTitleChange(e.target.value) }
+          readOnly={readOnly}
+        />
         <a href="#">
-          <BsPencilFill size={24} className="text-neutral-400" />
+          <BsPencilFill size={24} className="text-neutral-400" onClick={handleEdit} />
         </a>
       </div>
       <div>
