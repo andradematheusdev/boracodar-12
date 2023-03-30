284ec8b1988b5fe2ad57ee3ac5eaff866aac210b
Heloriel <andradematheus.dev@gmail.com>
2023-03-30 12:24:35 -0300

    Edit title on click

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,53 +9,67 @@ export const Header = () => {
   const [readOnly, setReadOnly] = useState(true);
   const titleEl = useRef<HTMLInputElement>(null);
   const [titleWidth, setTitleWidth] = useState<number | undefined>();
-  
+
   useEffect(() => {
-    if(titleEl.current!.value.length < 8){
+    if (titleEl.current!.value.length < 8) {
       setTitleWidth(8);
-    }else if(titleEl.current!.value.length >= 28){
-        setTitleWidth(28);
-    }else{
+    } else if (titleEl.current!.value.length >= 28) {
+      setTitleWidth(28);
+    } else {
       setTitleWidth(titleEl.current!.value.length);
     }
-  }, [title])
+  }, [title]);
 
-  function handleEdit(){
-      setReadOnly(!readOnly);    
+  function handleEdit() {
+    setReadOnly(false);
   }
 
-  function handleTitleChange(text: string){
+  function handleTitleChange(text: string) {
     setTitle(text);
-  }  
+  }
+
+  function handleBlur() {
+    setReadOnly(true);
+  }
 
   return (
     <header className="flex w-full justify-between items-center px-6 py-3">
       <div className="flex flex-1 items-center">
         <input
           type={"text"}
-          className={
-            clsx({
-              "border-b": !readOnly},
-              `flex bg-transparent text-4xl font-bold text-neutral-700 focus:outline-none p-0`,
-            )}
+          className={clsx(
+            {
+              "border-b": !readOnly,
+            },
+            `flex bg-transparent text-4xl font-bold text-neutral-700 focus:outline-none p-0`,
+          )}
           value={title}
           onChange={(e) => handleTitleChange(e.target.value)}
           readOnly={readOnly}
-          style={{width: `${titleWidth}ch`}}
+          style={{ width: `${titleWidth}ch` }}
           placeholder={"Sem nome"}
           ref={titleEl}
           maxLength={32}
+          onBlur={handleBlur}
+          onClick={handleEdit}
         />
-        {/* {title.length} */}
         <a href="#" className="">
-          {
-            readOnly
-            ? <BsPencilFill size={24} className="text-neutral-400" onClick={handleEdit} />
-            : <BsSave size={24} className="text-neutral-400" onClick={handleEdit} />
-          }
+          {readOnly ? (
+            <BsPencilFill
+              size={24}
+              className="text-neutral-400"
+              onClick={handleEdit}
+            />
+          ) : (
+            <BsSave
+              size={24}
+              className="text-neutral-400"
+              onClick={handleEdit}
+            />
+          )}
         </a>
       </div>
       <Avatar picture="https://github.com/andradematheusdev.png" username="Matheus Andrade" />
     </header>
-  )
-}
+  );
+};
