d56437cd0dee05e49b145b103e94dba8a2beeb85
Heloriel <andradematheus.dev@gmail.com>
2023-04-05 20:21:55 -0300

    Fix: search bar height

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -2,12 +2,12 @@ import { BsFilter, BsSearch } from "react-icons/bs";
 
 export const Search = () => {
   return (
-    <div className="flex w-full items-stretch gap-x-4 px-6 h-16 my-4">
-      <button className="flex items-center justify-center text-white bg-violet-600 rounded w-36 text-sm gap-x-2 hover:bg-violet-700 leading-none">
+    <div className="flex w-full items-stretch gap-x-4 px-6 my-4">
+      <button className="flex items-center justify-center text-white bg-violet-600 rounded w-36 text-sm gap-x-2 hover:bg-violet-700 leading-none h-12">
         <BsFilter size={22} />
         <span>Filtrar</span>
       </button>
-      <div className="flex flex-1 items-center border border-neutral-200 rounded-lg overflow-hidden px-7 bg-white">
+      <div className="flex flex-1 items-center border border-neutral-200 rounded-lg overflow-hidden px-7 bg-white h-12">
         <BsSearch size={18} />
         <input
           type="text"
