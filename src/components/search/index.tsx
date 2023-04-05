23d8bd9241b44e7c3f6bc9ab825dd790defcfb88
Heloriel <andradematheus.dev@gmail.com>
2023-04-05 20:37:12 -0300

    Hidden search bar in small screens

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -2,7 +2,7 @@ import { BsFilter, BsSearch } from "react-icons/bs";
 
 export const Search = () => {
   return (
-    <div className="flex w-full items-stretch gap-x-4 px-6 my-4">
+    <div className="hidden sm:flex w-full items-stretch gap-x-4 px-6 my-4">
       <button className="flex items-center justify-center text-white bg-violet-600 rounded w-36 text-sm gap-x-2 hover:bg-violet-700 leading-none h-12">
         <BsFilter size={22} />
         <span>Filtrar</span>
